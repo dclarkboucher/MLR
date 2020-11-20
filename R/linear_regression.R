c745be7dfee4fdeef0f1f32bdce318d4dda30e37
Dylan Clark-Boucher <clarkbou@umich.edu>
2020-11-20 12:46:46 -0500

    commit 11/20

--- R/linear_regression.R
+++ R/linear_regression.R
@@ -1,25 +1,22 @@
+#' regression_fit
+#'
+#' Fits multiple regression model
+#'
+#' @param formula an object of class "formula" or one that can be coerced by as.formula
+#' @param data an object of class "data.frame" or one that can be coerced by as.data.frame.
+#'
+#' @return A list containing the model formula, fitted values, a table of coefficients with test results, and the results of an ANOVA test.
+#'
+#' @examples
+#' data(mtcars)
+#' model_fit <- regression_fit(formula = mpg ~ wt + hp + wt:hp, data = mtcars)
+#'
+#'
+#' @export
 
-#formula_full. Useful if formula of interest is nested in a full model.
-#Complete cases will be selected based on full model so that no
-#extra observations are included in analysis. Argument omitted if not nested.
-
-regression_fit <- function(formula, data, formula_full = formula){
-
-  data <- mtcars
-  formula <- formula(mpg ~ disp + I(disp^2) + wt + as.factor(gear))
-
-  #If formula full does not contain formula, ignore
-  if(! all((all.vars(formula) %in% all.vars(formula_full)))){
-    formula_full <- formula
-    print("Notice: Full formula does not contain true formula. Argument omitted.")
-  }
-
-  #Subset data
+regression_fit <- function(formula, data){
+  formula <- as.formula(formula)
   data <- as.data.frame(data)
-  data <- data[,all.vars(formula_full)]
-  row_select <- complete.cases(data)
-  data <- data[row_select,all.vars(formula)]
-
   #Produce X and Y objects
   x <- model.matrix(formula,data)
   y <- model.response(model.frame(formula,data))
@@ -32,20 +29,6 @@ regression_fit <- function(formula, data, formula_full = formula){
   xtx_inv_xt <- xtx_inv %*% xt
   beta <- xtx_inv_xt %*% y
 
-  #Test estimates
-  se_beta <- sqrt(colSums(var_covar * diag(p)))
-  testStat <- beta / se_beta
-  pvals <- 2*(1 - pt(abs(testStat), n-p))
-
-  coefs <- matrix(data = NA, nrow = 4, ncol = p)
-  colnames(coef) <- c("Estimate", "Std. Error", "t value", "Pr(>|t|)")
-  rownames(coef) <- colnames(x)
-
-  coefs[["Estimates"]] <- beta
-  coefs[["Std. Error"]] <- se_beta
-  coefs[["t value"]] <- testStat
-  coefs[["Pr(>|t|)"]] <- pvals
-
   #Analysis of variance
   fitted_values <- x %*% beta
   residuals <- y - fitted_values
@@ -57,7 +40,20 @@ regression_fit <- function(formula, data, formula_full = formula){
   ssy <- sum((y - my)^2)
   ssr <- ssy - sse
 
-  var_covar <- MSE * xtx_inv
+  var_covar <- mse * xtx_inv
+
+  #Test estimates
+  se_beta <- sqrt(colSums(var_covar * diag(p)))
+  testStat <- beta / se_beta
+  pvals <- 2*(1 - pt(abs(testStat), n-p))
+  coefs <- as.data.frame(matrix(data = NA, nrow = p, ncol = 4))
+  colnames(coefs) <- c("Estimate", "Std. Error", "t value", "Pr(>|t|)")
+  rownames(coefs) <- colnames(x)
+
+  coefs[["Estimates"]] <- beta
+  coefs[["Std. Error"]] <- se_beta
+  coefs[["t value"]] <- testStat
+  coefs[["Pr(>|t|)"]] <- pvals
 
   #R-squared
   rsq <- ssr/ssy
@@ -68,10 +64,10 @@ regression_fit <- function(formula, data, formula_full = formula){
   outList <- c()
   outList$Formula <- formula
   outList$Residuals <- residuals
-  outList$Fitted_values <- yfit
+  outList$Fitted_values <- fitted_values
   outList$Coefficients <- pvals
   outList$Variance_covariance <- var_covar
-  outList$R_squared <- c(r_sq,r_sq_adj)
-
+  outList$R_squared <- c(rsq,rsq_adj)
+  return(3)
 }
 
